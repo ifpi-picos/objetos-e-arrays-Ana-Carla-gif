771452eff9f64ba9cfc6463e2b0f1fc3f0b38627
Ana_Carla_Guimarães <annacarlabarbosaguimaraes49@gmail.com>
2024-07-23 10:01:55 -0300

    Atividade

---
+++ arrayex/ex10.js
@@ -0,0 +1,6 @@
+const filmes = ["Ilha do Medo", "A lagoa Azul", "Descendentes", "A Mulher na Janela", "Barbie e As Três Mosqueteiras"];
+
+console.log("Filmes informados:");
+for (let i = 0; i < filmes.length; i++) {
+    console.log(filmes[i]);
+}
